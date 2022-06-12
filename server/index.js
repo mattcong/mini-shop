44256fadc83374c9bddc3f0886d5b8196ffaccb0
Matt Congdon <mattcongdon95@gmail.com>
2022-06-12 17:29:03 +0100

    local start

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
 app.use(express.static(DIST_DIR))
 app.use(session({
-    secret: process.env.SECRET_KEY,
+    secret: process.env.SECRET_KEY || "secret-key",
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 6000000 }
